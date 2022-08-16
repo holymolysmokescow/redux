c8d98d786547e88a29762149138c36f5996b412a
atharva <demo@user.com>
2022-08-16 09:04:52 +0530

    first commit | added boiler plate starter code

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { createStore } from 'redux';
+
+ReactDOM.render(<h1>Hello threre from redux</h1>, document.getElementById('root'));
